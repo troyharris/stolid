969de526b844fee36e4f881b33011e1d2ed123ac
Troy HARRIS <troy.harris@djt-tech.com>
2013-07-31 16:54:15 -0700

    turned parse code into a function

--- parse.go
+++ parse.go
@@ -6,10 +6,10 @@ import (
 		"os"
 )
 
-func main() {
-	p := markdown.NewParser(&markdown.Extensions{Smart: true})
+func parseFile(infile string, outfile string) {
+		p := markdown.NewParser(&markdown.Extensions{Smart: true})
 
-   	fi, err := os.Open("test.md")
+   	fi, err := os.Open(infile)
     if err != nil { panic(err) }
     // close fi on exit and check for its returned error
     defer func() {
@@ -21,22 +21,23 @@ func main() {
     r := bufio.NewReader(fi)
 
    // open output file
-    fo, err := os.Create("test.html")
+    fo, err := os.Create(outfile)
     if err != nil { panic(err) }
     // close fo on exit and check for its returned error
     defer func() {
         if err := fo.Close(); err != nil {
             panic(err)
         }
-    }()
+    }() 
+
     // make a write buffer
     w := bufio.NewWriter(fo)
 
     p.Markdown(r, markdown.ToHTML(w))
 
     if err = w.Flush(); err != nil { panic(err) }
+}
 
-	//	w := bufio.NewWriter(os.Stdout)
-	//	p.Markdown(os.Stdin, markdown.ToHTML(w))
-	//	w.Flush()
-	}
+func main() {
+	parseFile("test.md", "test.html");
+}
