7ac509bdae3a63e061a9c65c40d6a54c480176c9
Troy HARRIS <troy.harris@djt-tech.com>
2013-08-01 14:08:54 -0700

    Added config file and support for specifying template and destination directory

--- parse.go
+++ parse.go
@@ -8,7 +8,27 @@ import (
 		"path/filepath"
 		"strings"
         "io/ioutil"
+        "encoding/json"
+        "path"
 )
+
+type Options struct {
+    ContentPath, DestPath, TemplatePath string
+}
+
+var config = Options{}
+
+func readConfig () {
+    configBytes := readFile("config.json")
+    _ = json.Unmarshal(configBytes, &config)
+    //fmt.Println(string(configBytes))
+}
+
+func readFile (file string) []byte {
+    b, err := ioutil.ReadFile(file)
+    if err != nil { panic(err) }
+    return b
+}
 
 func parseFile (name string, infile string, outfile string) {
     top, bottom := buildPage(name)
@@ -26,6 +46,9 @@ func parseFile (name string, infile string, outfile string) {
     r := bufio.NewReader(fi)
 
    // open output file
+    newPath := path.Dir(outfile)
+    _ = os.MkdirAll(newPath, 0774)
+
     fo, err := os.Create(outfile)
     if err != nil { panic(err) }
     // close fo on exit and check for its returned error
@@ -44,30 +67,17 @@ func parseFile (name string, infile string, outfile string) {
 
     if err = w.Flush(); err != nil { panic(err) }
 }
-/*
-
-func markdownToHTML (infile string) string {
-    p := markdown.NewParser(&markdown.Extensions{Smart: true})
-
-    fi, err := os.Open(infile)
-    if err != nil { panic(err) }
-    // close fi on exit and check for its returned error
-    defer func() {
-        if err := fi.Close(); err != nil {
-            panic(err)
-        }
-    }()
-
-    r := bufio.NewReader(fi)
-    return "hi"
-}
-*/
 
 func dirWalk (path string, info os.FileInfo, err error) error {
 	if info.IsDir() == false && filepath.Ext(path) == ".md"{
 		fileroot := strings.Split(info.Name(), ".md")[0]
-		htmlfile := filepath.Dir(path) + "/" + fileroot + ".html"
-		fmt.Printf("Found %s\n", fileroot)
+        outtail := strings.Split(path, config.ContentPath)[1]
+        fmt.Println(outtail)
+        outdir := filepath.Dir(outtail)
+      //  fmt.Println(path)
+		htmlfile := config.DestPath + outdir + "/" + fileroot + "/index.html"
+	//	fmt.Printf("Found %s\n", fileroot)
+        fmt.Println(htmlfile)
 		parseFile("index", path, htmlfile)
 	}
 	return nil
@@ -88,6 +98,8 @@ func buildPage (name string) (string, string) {
 
 
 func main() {
-	root := "content"
+    readConfig()
+    fmt.Printf("DestPath is %s and TemplatePath is %s", config.DestPath, config.TemplatePath)
+	root := config.ContentPath
 	_ = filepath.Walk(root, dirWalk)
 }
