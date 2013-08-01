959f3c58bcd662fd0b6b34e16025bcebed2143a4
Troy HARRIS <troy.harris@djt-tech.com>
2013-07-31 17:25:54 -0700

    added directory walking and parsing function

--- parse.go
+++ parse.go
@@ -4,9 +4,12 @@ import (
 		"github.com/knieriem/markdown"
 		"bufio"
 		"os"
+		"fmt"
+		"path/filepath"
+		"strings"
 )
 
-func parseFile(infile string, outfile string) {
+func parseFile (infile string, outfile string) {
 		p := markdown.NewParser(&markdown.Extensions{Smart: true})
 
    	fi, err := os.Open(infile)
@@ -38,6 +41,18 @@ func parseFile(infile string, outfile string) {
     if err = w.Flush(); err != nil { panic(err) }
 }
 
+func dirWalk (path string, info os.FileInfo, err error) error {
+	if info.IsDir() == false && filepath.Ext(path) == ".md"{
+		fileroot := strings.Split(info.Name(), ".md")[0]
+		htmlfile := filepath.Dir(path) + "/" + fileroot + ".html"
+		fmt.Printf("Found %s\n", fileroot)
+		parseFile(path, htmlfile)
+	}
+	return nil
+}
+
 func main() {
 	parseFile("test.md", "test.html");
+	root := "."
+	_ = filepath.Walk(root, dirWalk)
 }
